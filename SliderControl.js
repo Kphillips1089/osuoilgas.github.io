26c98b18f0c51046f8f0013292be7c340f9c71f1
Nick Tocicki <runnerdude1515@MacBook-Pro.local>
2017-11-13 16:28:08 -0500

    Added popups and colors for each mining type

--- SliderControl.js
+++ SliderControl.js
@@ -13,7 +13,7 @@ L.Control.SliderControl = L.Control.extend({
         range: false,
         follow: false,
         sameDate: false,
-        alwaysShowDate: false,
+        alwaysShowDate: true,
         rezoom: 0
     },
 
